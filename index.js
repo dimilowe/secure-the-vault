c4a2f7cc5a27e7ed8a3e94521528c6f8a9f63b6d
Dimitrios Lowe <dimitrioslowe@gmail.com>
2025-05-15 08:43:25 -0400

    initial submit, needs fixes

---
+++ index.js
@@ -0,0 +1,18 @@
+const x = code1;
+code1 = 7 + 3; // 10
+
+code2 = (130 - 10) / 30; //40
+
+code3 = 38 + (10 % 3); // 39
+
+message = "The vault has been secured. The combination is:";
+
+codeA = "code1" + "code2" + "code3";
+
+codeB = `${code1} - ${code2} - ${code3}`;
+
+console.log(message);
+
+console.log(codeA);
+
+console.log(codeB);
