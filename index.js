9917c91056e012f23d15d2f3b6e225802c92f3b9
Dimitrios Lowe <dimitrioslowe@gmail.com>
2025-05-15 10:00:42 -0400

    2nd submission, less issues

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const x = code1;
-code1 = 7 + 3; // 10
+const code1 = 7 + 3; // 10
 
-code2 = (130 - 10) / 30; //40
+code2 = (130 - 10) / 3; //40
 
 code3 = 38 + (10 % 3); // 39
 
 message = "The vault has been secured. The combination is:";
 
-codeA = "code1" + "code2" + "code3";
+codeA = `code1` + `code2` + `code3`;
 
 codeB = `${code1} - ${code2} - ${code3}`;
 
